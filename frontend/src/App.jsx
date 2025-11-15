d119ef2f1320a80b214057f7dbed00ecb7bb5319
SriganeshKamath <sriganeshkamath1@gmail.com>
2025-11-16 00:10:43 +0530

    Changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Welcome from "./pages/Welcome";
 import Signup from "./pages/signup";
 import Dashboard from "./pages/dashboard";
+import Lesson from "./pages/lesson";
 
 export default function App() {
   return (
@@ -11,6 +12,7 @@ export default function App() {
         
         <Route path="/signup" element={<Signup />} />
         <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/lesson" element={<Lesson />} />
 
       </Routes>
     </BrowserRouter>
